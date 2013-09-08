6f67ec4c94abf20285f7a842d2a25ff8c383e6aa
czp <zuopingchen@gmail.com>
2013-09-08 12:11:16 +0800

    small changes

--- src/main/java/com/yihaodian/sby/console/ResourceUtil.java
+++ src/main/java/com/yihaodian/sby/console/ResourceUtil.java
@@ -4,25 +4,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringWriter;
-import java.util.HashMap;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.codehaus.groovy.control.CompilationFailedException;
-import org.codehaus.groovy.control.CompilerConfiguration;
-import org.springframework.core.io.Resource;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-
-import freemarker.cache.StringTemplateLoader;
-import freemarker.template.Configuration;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
-import groovy.lang.Binding;
-import groovy.lang.GroovyClassLoader;
-import groovy.lang.GroovyShell;
 
 public class ResourceUtil{
 	public static String getFileAsString(String name){
